560068d540f8a3547c602b6f5c9b01e88de83584
Carlos Payá <carlos.paya@csic.es>
2024-09-03 16:26:54 +0200

    test

--- calcs/calc_Jdisorder.jl
+++ calcs/calc_Jdisorder.jl
@@ -32,7 +32,7 @@ function calc_mismatch_J(modL, modR, σ; Brng = subdiv(0.0, 0.25, 100), φs = su
     g_right, g_left, g = greens_dict[gs](hSC_left, hSC_right, params_left, params_right)
 
     bandwidth = maximum([model_left.Δ0, model_right.Δ0]) * 50
-    J = josephson(g[attach_link[gs]], bandwidth; imshift = 1e-3, omegamap = ω -> (; ω), phases = φs, atol = 1e-3)
+    J = josephson(g[attach_link[gs]], bandwidth; imshift = 1e-5, omegamap = ω -> (; ω), phases = φs, atol = 1e-5)
     #J = josephson(g[attach_link[gs]], 0.23 * 50; imshift = 1e-5, omegamap = ω -> (; ω), phases = φs, atol = 1e-5)
 
     Js_τ = Js_flux(J, Brng, τs)
