9e57e9fe96a30ee675ad5c6b31a5e4422767b1bb
Carlos Payá <carlos.paya@csic.es>
2024-09-09 10:56:58 +0200

    reduced range

--- mods/params.jl
+++ mods/params.jl
@@ -1,5 +1,5 @@
 @with_kw struct Calc_Params
-    Brng = subdiv(0.0, 0.25, 400)
+    Brng = subdiv(0.0, 0.25, 300)
     ωrng = subdiv(-.26, .26, 401) .+ 1e-3im
     φrng = subdiv(0, 2π, 101)
     outdir = "Results"
