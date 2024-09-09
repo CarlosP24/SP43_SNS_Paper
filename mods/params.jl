82e2c4ea113f909d76d44624b98358e28584cf13
Carlos Payá <carlos.paya@csic.es>
2024-09-09 11:41:22 +0200

    increase tolerance, not imshift

--- mods/params.jl
+++ mods/params.jl
@@ -1,7 +1,7 @@
 @with_kw struct Calc_Params
-    Brng = subdiv(0.0, 0.25, 200)
+    Brng = subdiv(0.0, 0.25, 400)
     ωrng = subdiv(-.26, .26, 401) .+ 1e-3im
-    φrng = subdiv(0, 2π, 41)
+    φrng = subdiv(0, 2π, 101)
     outdir = "Results"
 end
 
