18b82ea48d2d42a9064cb650c8d1d8ea3b45a564
Carlos Payá <carlos.paya@csic.es>
2024-09-12 18:01:17 +0200

    more points for diode

--- mods/params.jl
+++ mods/params.jl
@@ -1,7 +1,7 @@
 @with_kw struct Calc_Params
-    Brng = subdiv(0.0, 0.25, 400)
+    Brng = subdiv(0.0, 0.25, 100)
     ωrng = subdiv(-.26, .26,  401) .+ 1e-3im
-    φrng = subdiv(0, 2π, 101)
+    φrng = subdiv(0, 2π, 51)
     outdir = "Results"
 end
 
