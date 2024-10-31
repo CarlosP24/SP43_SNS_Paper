63b90928cf8ce17140fcf52a914b86b5cfb5a3c9
Carlos Payá <carlos.paya@csic.es>
2024-10-31 13:08:20 +0100

    debug

--- src/models/params.jl
+++ src/models/params.jl
@@ -1,6 +1,6 @@
 @with_kw struct Calc_Params
     Brng = subdiv(0.0, 0.25, 200)
-    ωrng = subdiv(-.26, 0,  201)
+    ωrng = subdiv(-.26, 0,  201) .+ 1e-4im
     φrng = subdiv(0, 2π, 101)
     imshift = 1e-5
     outdir = "data"
