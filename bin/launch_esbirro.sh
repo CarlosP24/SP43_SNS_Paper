57ee1b68411a1b29e2f0cfe12ecbd9e67f1b3eb7
Carlos Payá <carlos.paya@csic.es>
2024-10-31 18:24:51 +0100

    debug mem

--- bin/launch_esbirro.sh
+++ bin/launch_esbirro.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source config/prologue.sh
-input = $1
 sbatch <<EOT
 #!/bin/bash
 ## Slurm header
@@ -8,9 +7,9 @@ sbatch <<EOT
 #SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+##SBATCH --mem-per-cpu=2G
 #SBATCH --output="logs/%j.out"
 #SBATCH --job-name="${PWD##*/}"
 
-julia --project bin/launcher.jl $input
+julia --project bin/launcher.jl "$@"
 EOT
