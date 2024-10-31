e7ae233a1200844a25fae19f99172912de22508e
Carlos Payá <carlos.paya@csic.es>
2024-10-31 16:40:59 +0100

    prepare for esbirro

--- bin/launch_drago.sh
+++ bin/launch_drago.sh
@@ -4,11 +4,13 @@ sbatch <<EOT
 #!/bin/bash
 ## Slurm header
 #SBATCH --partition=special
-#SBATCH --ntasks=96
+#SBATCH --ntasks=500
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=3G
 #SBATCH --output="logs/%j.out"
 #SBATCH --job-name="${PWD##*/}"
+#SBATCH --mail-user=carlos.paya@csic.es
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
 
-julia --project bin/launcher.jl
+julia --project bin/launcher.jl "$@"
 EOT
