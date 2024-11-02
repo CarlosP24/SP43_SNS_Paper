87fad46cdd8e907be5217aa691b90eb2ff708efc
Carlos Payá <carlos.paya@csic.es>
2024-11-02 22:44:48 +0100

    debug

--- src/calculations/Josephson.jl
+++ src/calculations/Josephson.jl
@@ -41,7 +41,7 @@ function calc_Josephson(name::String, calc_params::Calc_Params)
     itipR = get_itip(; wireR...)
     itip(B) = minimum([itipL(B), itipR(B)])
     ipath(B) = [-bw, -wireL.Δ0,  -wireL.Δ0/2 + itip(B)*1im, 0] .+ imshift*1im
-    J1 = josephson(g[attach_link[gs]], ipath(0); omegamap = ω -> (; ω), phases = φrng, atol = 1e-7, maxevals = 10^6, order = 21, )
+    J1 = josephson(g[attach_link[gs]], ipath(0); omegamap = ω -> (; ω), phases = φrng, atol = 1e-7, maxevals = 10^6, order = 21, callback = (x, y) -> @show x )
 
     # Compute Josephson current
     Js = pjosephson([J1], Brng, length(φrng), ipath; τ, hdict)
