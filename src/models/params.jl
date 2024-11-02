4d0060afcaf1a80555a467a10c72361254b523b5
Carlos Payá <carlos.paya@csic.es>
2024-11-02 21:16:15 +0100

    josephson testing

--- src/models/params.jl
+++ src/models/params.jl
@@ -1,5 +1,5 @@
 @with_kw struct Calc_Params
-    Brng = subdiv(0.0, 0.25, 400)
+    Brng = subdiv(0.0, 0.25, 200)
     ωrng = subdiv(-.26, 0,  201) .+ 1e-3im
     φrng = subdiv(0, 2π, 101)
     imshift = 1e-5
