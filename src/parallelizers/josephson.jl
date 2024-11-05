3f992f3a2d2b034ce4e916787adba554675a83a0
Carlos Payá <carlos.paya@csic.es>
2024-11-05 16:19:41 +0100

    test

--- src/parallelizers/josephson.jl
+++ src/parallelizers/josephson.jl
@@ -6,7 +6,7 @@ lg is the length of the φrng inside J. Needed for error handling purposes.
     pjosephson(J, Brng, τs; hdict = Dict(0 => 1, 1 => 0.1))
 Compute the Josephson current from J::Josephson integrator for a set of magnetic fields and junction transmissions, given noise harmonics hdict.
 """
-function pjosephson(Js, Brng, lg::Int, ipaths::Vector{Function}; τ = 1,  hdict = Dict(0 => 1, 1 => 0.1))
+function pjosephson(Js::Vector{Any}, Brng, lg::Int, ipaths::Vector{Function}; τ = 1,  hdict = Dict(0 => 1, 1 => 0.1))
     Jss = @showprogress pmap(Brng) do B
         # t0 = time()
         # j = @async sum([J(override_path = ipath(B); B, τ , hdict, ) for J in Js])
@@ -28,29 +28,29 @@ function pjosephson(Js, Brng, lg::Int, ipaths::Vector{Function}; τ = 1,  hdict 
     return reshape(Jss, size(Brng)...)
 end
 
-function pjosephson(J, Brng, τs;  hdict = Dict(0 => 1, 1 => 0.1))
-    pts = Iterators.product(Brng, τs)
-    lg = length(J())
-    Jss = @showprogress pmap(pts) do pt
-        B, τ = pt
-        j = try 
-            J(; B, τ , hdict)
-        catch
-            [NaN for _ in 1:Int(lg)]
-        end
-        return j
-    end
-    Barray = reshape(Jss, size(pts)...)
-    return Dict([τ => Barray[:, i] for (i, τ) in enumerate(τs)])
-end
+# function pjosephson(J, Brng, τs;  hdict = Dict(0 => 1, 1 => 0.1))
+#     pts = Iterators.product(Brng, τs)
+#     lg = length(J())
+#     Jss = @showprogress pmap(pts) do pt
+#         B, τ = pt
+#         j = try 
+#             J(; B, τ , hdict)
+#         catch
+#             [NaN for _ in 1:Int(lg)]
+#         end
+#         return j
+#     end
+#     Barray = reshape(Jss, size(pts)...)
+#     return Dict([τ => Barray[:, i] for (i, τ) in enumerate(τs)])
+# end
 
-function pjosephson_g(g, Brng, φrng, ipath; τ = 1,  hdict = Dict(0 => 1, 1 => 0.1),)
-    pts = Iterators.product(Brng, φrng)
-    Jss = @showprogress pmap(pts) do pt
-        B, φ = pt
-        J = josephson(g, ipath(B);  omegamap = ω -> (; ω), phases = [φ], atol = 1e-7, maxevals = 10^6, order = 21,)
-        return J( ; B, τ , hdict, )
-    end
-    return reshape(Jss, size(pts)...)
-end
+# function pjosephson_g(g, Brng, φrng, ipath; τ = 1,  hdict = Dict(0 => 1, 1 => 0.1),)
+#     pts = Iterators.product(Brng, φrng)
+#     Jss = @showprogress pmap(pts) do pt
+#         B, φ = pt
+#         J = josephson(g, ipath(B);  omegamap = ω -> (; ω), phases = [φ], atol = 1e-7, maxevals = 10^6, order = 21,)
+#         return J( ; B, τ , hdict, )
+#     end
+#     return reshape(Jss, size(pts)...)
+# end
 
