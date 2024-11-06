9ef4ba9aab5de2e0169831633885ac7730538168
Carlos Payá <carlos.paya@csic.es>
2024-11-06 15:07:59 +0100

    test

--- bin/launch_esbirro.sh
+++ bin/launch_esbirro.sh
@@ -23,6 +23,6 @@ PARAMS=("$@")
 # Access the parameter for this specific job based on SLURM_ARRAY_TASK_ID
 PARAM="${PARAMS[$SLURM_ARRAY_TASK_ID]}"
 
-julia --project bin/launcher.jl $PARAM
+julia --project bin/launcher.jl "$PARAM"
 
 EOT
