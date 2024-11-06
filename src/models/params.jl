834ea19648af1fc3d00cb9528b2dc2e421b8fb4d
Carlos Payá <carlos.paya@csic.es>
2024-11-06 22:22:24 +0100

    more mJ modes for reference calculations

--- src/models/params.jl
+++ src/models/params.jl
@@ -1,7 +1,7 @@
 @with_kw struct Calc_Params
     Brng = subdiv(0.0, 0.25, 200)
     ωrng = subdiv(-.26, 0,  101) .+ 1e-3im
-    φrng = subdiv(0, 2π, 51)
+    φrng = subdiv(0, 2π, 101)
     imshift = 1e-4
     outdir = "data"
 end
