6709f550d698b8ea369ff7ab2e782cd9f9da0ef6
Carlos Payá <carlos.paya@csic.es>
2024-11-06 10:16:51 +0100

    debug ipaths

--- src/calculations/Josephson.jl
+++ src/calculations/Josephson.jl
@@ -46,8 +46,10 @@ function calc_Josephson(name::String, calc_params::Calc_Params)
     itipL = get_itip(; wireL...)
     itipR = get_itip(; wireR...)
     itip(B) = minimum([itipL(B), itipR(B)])
-    ipath1(B) = [-bw, -wireL.Δ0,  -wireL.Δ0/2 + itip(B)*1im, 0] .+ imshift*1im      # + imshift means retarded Greens. Advanced have a branch cut.
-    ipath2(B) = [-bw, -wireL.Δ0,  -wireL.Δ0/2 - itip(B)*1im, 0] .- imshift*1im      # - imshift means advanced Greens. Retarded have a branch cut.
+    let bw = bw, Δ0 = wireL.Δ0, itip = itip, imshift = imshift
+        ipath1(B) = [-bw, -Δ0,  -Δ0/2 + itip(B)*1im, 0] .+ imshift*1im      # + imshift means retarded Greens. Advanced have a branch cut.
+        ipath2(B) = [-bw, -Δ0,  -Δ0/2 - itip(B)*1im, 0] .- imshift*1im      # - imshift means advanced Greens. Retarded have a branch cut.
+    end
     @info "Path 1 is a $(typeof(ipath1)), ipath1(0) = $(ipath1(0))"
     J1 = josephson(g[attach_link[gs]], ipath1(0); omegamap = ω -> (; ω), phases = φrng1, atol = 1e-4, maxevals = 1e4, order = 21,)
     J2 = josephson(g[attach_link[gs]], ipath2(0); omegamap = ω -> (; ω), phases = φrng2, atol = 1e-4, maxevals = 1e4, order = 21,)
