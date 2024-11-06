8d6fb86efe4dd980d6b4954de4ecc580180695ea
Carlos Payá <carlos.paya@csic.es>
2024-11-06 15:51:00 +0100

    test

--- bin/launch_esbirro.sh
+++ bin/launch_esbirro.sh
@@ -3,14 +3,20 @@ source config/prologue.sh
 if [ $? -ne 0 ]; then
   exit 1
 fi
-# Define the array
-PARAMS=("reference_metal_1" "reference_dep_1")
+# Define parameters array
+PARAMS=("$@")
+ARRAY_SIZE=${#PARAMS[@]}
+
+if [ $ARRAY_SIZE -eq 0 ]; then
+  echo "No parameters provided"
+  exit 1
+fi
 
 # Serialize the array into a string
 PARAMS_STR=$(IFS=,; echo "${PARAMS[*]}")
-
 export PARAMS_STR
-# Store all command-line arguments in an array
+
+# Launch the job
 sbatch --export=ALL <<EOT
 #!/bin/bash
 ## Slurm header
@@ -19,18 +25,20 @@ sbatch --export=ALL <<EOT
 #SBATCH --nodes=2
 #SBATCH --cpus-per-task=1
 ##SBATCH --mem-per-cpu=2G
-#SBATCH --output="logs/%j.out"
-#SBATCH --job-name="${PWD##*/}_$1"
+#SBATCH --output="logs/%A_%a.out"
+#SBATCH --job-name="${PWD##*/}_$ARRAY_SIZE"
 #SBATCH --mail-user=carlos.paya@csic.es
 #SBATCH --mail-type=END,FAIL
-#SBATCH --array=1-2
+#SBATCH --array=1-$ARRAY_SIZE
 
+# Deserialize
 IFS=, read -r -a PARAMS <<< "\$PARAMS_STR"
 
+# Select the parameter
 PARAM="\${PARAMS[\$SLURM_ARRAY_TASK_ID-1]}"
+echo "Running \$PARAM"
 
-echo "Running with PARAM: \$PARAM"
-
+# Run the job
 julia --project bin/launcher.jl "\$PARAM"
 
 EOT
