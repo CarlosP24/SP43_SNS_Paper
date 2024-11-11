6cd79fc4021f9fc4bff0583b25d52101ad7a2ebb
Carlos Payá <carlos.paya@csic.es>
2024-11-11 17:24:54 +0100

    test

--- plots/plotters/fig_metal.jl
+++ plots/plotters/fig_metal.jl
@@ -24,12 +24,12 @@ function fig_metal(LDOS_left, LDOS_right, Is)
 
     ianalytic,  gap_L, gap_R = KO1(Is)
 
-    lines!(ax, tleft.Brng, gap_L.(tleft.Brng).^2; color = :lightblue, label = L"\Delta_L / \Delta_{L0}")
+    lines!(ax, tleft.Brng, gap_L.(tleft.Brng).^2; linestyle = :dash, color = :navyblue, label = L"\Delta_L / \Delta_{L0}")
     #lines!(ax, tright.Brng, gap_R.(tright.Brng); linestyle = :dash, color = :orange, label = L"\Delta_R / \Delta_{R0}")
 
     #cross_Δ = find_zeros(B -> gap_L(B) - gap_R(B), 0, 0.26)
 
-    lines!(ax, tleft.Brng, ianalytic.(tleft.Brng)./ianalytic.(0); linestyle = :dash, color = :navyblue, label = L"\text{Sherril}")
+    #lines!(ax, tleft.Brng, ianalytic.(tleft.Brng)./ianalytic.(0); linestyle = :dash, color = :navyblue, label = L"\text{Sherril}")
     #lines!(ax, tleft.Brng, gap_L.(tleft.Brng).^2 ./ gap_L(0)^2)
         #vlines!(ax, cross_Δ; linestyle = :dash, color = :black)
 
--- src/models/params.jl
+++ src/models/params.jl
@@ -8,7 +8,7 @@ end
 @with_kw struct J_Params
     imshift = 1e-4
     atol = 1e-9
-    maxevals = 1e8  
+    maxevals = 1e9  
     order = 21
 end
 
