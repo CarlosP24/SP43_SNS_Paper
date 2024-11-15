53b1d05189b7f754395c3426e16a327d8247d83a
Carlos Payá <carlos.paya@csic.es>
2024-11-15 16:50:59 +0100

    new TN

--- src/parallelizers/normal.jl
+++ src/parallelizers/normal.jl
@@ -8,3 +8,18 @@ function get_TN(G, τrng; kw...)
     end
     return reshape(Gτ, size(τrng)...)
 end
+
+function get_TN(hleft, hright, params_left, params_right, gs, τrng; kw...)
+    hs = []
+    for (i, h, params) in enumerate(zip([hleft, hright], [params_left, params_right]))
+        @unpack τΓ, Δ0 = params
+        ΣS! = @onsite!((o, r; ) -> o - Δ0 * τΓ * im)
+        hs[i] = h |> ΣS!
+    end
+    g_right, g_left, g = greens_dict[gs](hs[1], hs[2], params_left, params_right;)
+    G = conductance(g[1, 1])
+    Gτ = pmap(τrng) do τ
+        G(0; τ, kw...)
+    end
+    return reshape(Gτ, size(τrng)...)
+end
