40adeadabf74bdbc0a0b0ac910c1599713f6b39a
Carlos Payá <carlos.paya@csic.es>
2024-11-19 09:35:14 +0100

    debug

--- src/parallelizers/josephson.jl
+++ src/parallelizers/josephson.jl
@@ -29,7 +29,7 @@ function pjosephson(Js, Φrng, Zs, lg::Int, ipaths; τ = 1,  hdict = Dict(0 => 1
             jvec = [sign(imag(ipath(Φ) |> first)) * J(override_path = ipath(Φ); Φ, Z, τ, hdict, ) for (J, ipath) in zip(Js, ipaths)]
             return vcat(jvec...)
         catch e 
-            @warn "An error ocurred at B=$B, Z=$Z. \n$e \nOutput is NaN."
+            @warn "An error ocurred at Φ=$Φ, Z=$Z. \n$e \nOutput is NaN."
             return [NaN for _ in 1:Int(lg)]
         end
         return j
