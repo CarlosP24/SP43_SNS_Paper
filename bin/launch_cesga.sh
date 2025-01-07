8018ec5ba1ae621eb1e4ad80963f5ceda29a54ce
Carlos Payá <carlos.paya@csic.es>
2025-01-07 12:08:33 +0100

    correct launcher errors

---
+++ bin/launch_cesga.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source config/prologue.sh "$@"
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+# Launch the job
+sbatch --export=ALL <<EOT
+#!/bin/bash
+## Slurm header
+#SBATCH --ntasks=200
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=7-00:00:00
+#SBATCH --output="logs/%A_%a.out"
+#SBATCH --job-name="${PWD##*/}_$ARRAY_SIZE"
+#SBATCH --mail-user=carlos.paya@csic.es
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT_80
+#SBATCH --array=1-$ARRAY_SIZE
+
+# Deserialize
+IFS=, read -r -a PARAMS <<< "\$PARAMS_STR"
+
+# Select the parameter
+PARAM="\${PARAMS[\$SLURM_ARRAY_TASK_ID-1]}"
+echo "Running \$PARAM"
+
+# Run the job
+julia --project bin/launcher.jl "\$PARAM"
+EOT
